f7ef59943ca8bd3e758e1d3e543a1ecf0a330923
Alex Robson <asrobson@gmail.com>
2012-09-13 14:46:56 -0500

    Fixing lame oversight bug

--- src/component.js
+++ src/component.js
@@ -7,40 +7,46 @@ module.exports = function( _, anvil ) {
 		activity: "identify",
 		
 		run: function( done ) {
-			anvil.fs.getFiles( "./components", "./components", function( files, directories ) {
-				anvil.log.event( "found " + directories.length + " components" );
-				var componentsBase = path.resolve( "./components" ),
-					metadata = [];
-				_.each( directories, function( directory ) {
-					var relativePath = directory.replace( componentsBase, "" );
-						componentPath = anvil.fs.buildPath( [ directory, "component.json" ] ),
-						component = require( componentPath ),
-						componentName = path.basename( directory ).replace( /component[-]/, "" );
-					var files = [];
-					if( component.main ) {
-						files.push( component.main );
-					}
-					if( component.styles ) {
-						files = files.concat( component.styles );
-					}
-					if( component.scripts ) {
-						files = files.concat( component.scripts );
-					}
-					var data = _.map( files, function( file ) {
-						file = anvil.fs.buildPath( [ directory, file ] );
-						var data = anvil.fs.buildFileData(
-								path.dirname( file ),
-								anvil.fs.buildPath( [ anvil.config.working, "src", "ext", componentName ] ),
-								file );
-						data.relativePath = anvil.fs.buildPath( [ "ext", relativePath ] ).replace( /component[-]/, "" );
-						return data;
-					} );
-					metadata = metadata.concat( data );
-				} );
-				metadata = _.uniq( metadata, false, function( x ) { return x.fullPath; } );
-				anvil.project.files = anvil.project.files.concat( metadata );
+			var componentsBase = path.resolve( "./components" ),
+				exists = anvil.fs.pathExists( componentsBase );
+			if( !exists ) {
 				done();
-			}, [], 0 );
+			} else {
+				anvil.fs.getFiles( componentsBase, componentsBase, function( files, directories ) {
+					anvil.log.event( "found " + directories.length + " components" );
+					var componentsBase = path.resolve( componentsBase ),
+						metadata = [];
+					_.each( directories, function( directory ) {
+						var relativePath = directory.replace( componentsBase, "" );
+							componentPath = anvil.fs.buildPath( [ directory, "component.json" ] ),
+							component = require( componentPath ),
+							componentName = path.basename( directory ).replace( /component[-]/, "" );
+						var files = [];
+						if( component.main ) {
+							files.push( component.main );
+						}
+						if( component.styles ) {
+							files = files.concat( component.styles );
+						}
+						if( component.scripts ) {
+							files = files.concat( component.scripts );
+						}
+						var data = _.map( files, function( file ) {
+							file = anvil.fs.buildPath( [ directory, file ] );
+							var data = anvil.fs.buildFileData(
+									path.dirname( file ),
+									anvil.fs.buildPath( [ anvil.config.working, "src", "ext", componentName ] ),
+									file );
+							data.relativePath = anvil.fs.buildPath( [ "ext", relativePath ] ).replace( /component[-]/, "" );
+							return data;
+						} );
+						metadata = metadata.concat( data );
+					} );
+					metadata = _.uniq( metadata, false, function( x ) { return x.fullPath; } );
+					anvil.project.files = anvil.project.files.concat( metadata );
+					done();
+				}, [], 0 );
+			}
 		}
 	} );
 };
